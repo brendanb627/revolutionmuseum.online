18c5f2bedc6f585bb22b121013f7a5568eafb11d
Brendan Bessman <bdbessman@gmail.com>
2024-11-23 17:23:18 -0800

    adds virtual museum and website structure

---
+++ src/Pages/homepage.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { VirtualMuseum } from "../Components/virtual-museum";
+
+export const HomePage = () => {
+  const images = [
+    // Front
+    {
+      position: [0, 0, 1.5],
+      rotation: [0, 0, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture1",
+    },
+    // Back
+    {
+      position: [-0.8, 0, -0.6],
+      rotation: [0, 0, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture2",
+    },
+    {
+      position: [0.8, 0, -0.6],
+      rotation: [0, 0, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture3",
+    },
+    // Left
+    {
+      position: [-1.75, 0, 0.25],
+      rotation: [0, Math.PI / 2.5, 0],
+      url: "logo512.png",
+    },
+    {
+      position: [-2.15, 0, 1.5],
+      rotation: [0, Math.PI / 2.5, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture4",
+    },
+    {
+      position: [-2, 0, 2.75],
+      rotation: [0, Math.PI / 2.5, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture5",
+    },
+    // Right
+    {
+      position: [1.75, 0, 0.25],
+      rotation: [0, -Math.PI / 2.5, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture6",
+    },
+    {
+      position: [2.15, 0, 1.5],
+      rotation: [0, -Math.PI / 2.5, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: "picture7",
+    },
+    {
+      position: [2, 0, 2.75],
+      rotation: [0, -Math.PI / 2.5, 0],
+      url: "https://photographylife.com/wp-content/uploads/2023/05/Nikon-Z8-Official-Samples-00002.jpg",
+      viewname: `picture8\nThis is a picture`,
+    },
+  ];
+
+  return (
+    <div className="App">
+      <h1><span className="redGradient">Revolutionists</span> VS. <span className="blueGradient">Loyalists</span></h1>
+      <div className="sect">
+        <h2 className="gradient-text">Overview</h2>
+        <p>
+          This is a virtual museum. It is a 3D space where you can view images
+          from
+        </p>
+      </div>
+      <div className="virtual-museum">
+        <VirtualMuseum images={images} />
+      </div>
+      <div className="sectcover">
+        <div className="sect">
+        <h2 className="gradient-text">Sources</h2>
+        <p>
+            The images used in this virtual museum are from the following sources:
+            www.example.com
+        </p>
+        </div>
+        </div>
+    </div>
+  );
+};
